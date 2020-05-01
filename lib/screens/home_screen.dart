a35ed2dcb65bd5d6174ae4022cb1ac13b3aa609a
Bartek Pacia <barpac02@gmail.com>
2020-05-01 15:12:51 +0200

    added Firebase integration

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,12 +9,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var firestore = Firestore.instance.collection("celebrities").getDocuments();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Hi, User"),
       ),
-      body: Center(
-        child: Text("Hi"),
+      body: Container(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection("celebrities").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("An error occurred");
+            }
+
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Text(snapshot.data.documents[index].data["name"]),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
