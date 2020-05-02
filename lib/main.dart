cc6d1a1f8928f3c7535b072610b4d503e8012e0a
Bartek Pacia <barpac02@gmail.com>
2020-05-02 03:20:24 +0200

    added integration with Firebase Cloud Messaging; prints to console but doesn't show up in Android system tray

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,56 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:zelrap/background_notifications.dart';
 import 'package:zelrap/screens/home/home_screen.dart';
 import 'package:zelrap/theme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ZelrapApp());
 }
 
-class MyApp extends StatelessWidget {
+class ZelrapApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _firebaseMessaging = FirebaseMessaging();
+
+    print("build APP");
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showNotificationDialog(context, message);
+      },
+      onBackgroundMessage: backgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        // _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        // _navigateToItemDetail(message);
+      },
+    );
+
     return MaterialApp(
       title: 'Zelpar',
       theme: lightTheme(context),
       darkTheme: darkTheme(context),
       home: HomeScreen(),
     );
+  }
+
+  void _showNotificationDialog(BuildContext context, Map<String, dynamic> message) async {
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {},
+    );
+
+    final alertDialog = AlertDialog(
+      title: Text(message["title"]),
+      content: Text(message["content"]),
+      actions: [okButton],
+    );
+
+    await showDialog(context: context, builder: (BuildContext context) => alertDialog);
   }
 }
