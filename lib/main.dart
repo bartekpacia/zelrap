c29e57dc9fc40cb2fb0dfc2596709d811737b873
Bartek Pacia <barpac02@gmail.com>
2021-03-29 15:33:29 +0200

    wooah, runs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:zelrap/data/models/notification_item.dart';
 import 'package:zelrap/screens/home/home_screen.dart';
 import 'package:zelrap/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(ZelrapApp());
 }
 
